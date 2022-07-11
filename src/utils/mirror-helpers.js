073009c1d9819a9597d90fa2d9cd18ac837219dd
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-11 18:04:14 +0300

    feat: more MirrorXYZ metadata

---
+++ src/utils/mirror-helpers.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export async function getMirrorCover(blog_url) {
+  try {
+    const FIRST_SREACH_LENGTH = 65;
+    const IMG_ID_LENGTH = 21;
+    const res = await axios.get(blog_url);
+    const start = res.data.indexOf(
+      `<meta content="https://mirror-media.imgix.net/publication-images/`
+    );
+    const img_id = res.data.slice(
+      start + FIRST_SREACH_LENGTH,
+      start + FIRST_SREACH_LENGTH + IMG_ID_LENGTH
+    );
+
+    const extensionStart = res.data.slice(
+      start + FIRST_SREACH_LENGTH + IMG_ID_LENGTH
+    );
+    const extensionFin = extensionStart.indexOf("?h");
+    const extension = extensionStart.slice(1, extensionFin);
+
+    const cover_url = `https://mirror-media.imgix.net/publication-images/${img_id}.${extension}`;
+    console.log(cover_url);
+    return cover_url;
+  } catch (error) {
+    return false;
+  }
+}
